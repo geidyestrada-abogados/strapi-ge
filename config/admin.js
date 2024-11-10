c6edcddc72e3e7e5226311af509b47dbac24aefb
geidyestrada-abogados <torra@automaticaintegral.com>
2024-11-10 02:00:01 -0500

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
