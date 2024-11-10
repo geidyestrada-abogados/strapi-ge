3c6d9722a784bf8c7a8bff3ef775ea90ba1677c8
geidyestrada-abogados <torra@automaticaintegral.com>
2024-11-10 02:16:59 -0500

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','k7lEP1lvp2DxTDpFSQORxA=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'k7lEP1lvp2DxTDpFSQORxA=='),
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
   flags: {
