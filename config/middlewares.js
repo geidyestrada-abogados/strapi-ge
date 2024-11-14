d063e1d261b5cc4ff4988ffb570f8a5a9797c2f6
geidyestrada-abogados <torra@automaticaintegral.com>
2024-11-14 02:49:16 -0500

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,6 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // Permite cualquier origen (menos seguro)
+      // Alternativamente, permite solo tu dominio en GitHub Pages:
+      // origin: ['https://geidyestrada-abogados.github.io'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
